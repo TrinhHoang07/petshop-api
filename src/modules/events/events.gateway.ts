7a4891e839a68cd8f351a865d6c725dc2f3f5796
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2023-07-02 19:35:13 +0700

    handle chat with client (no login)

--- src/modules/events/events.gateway.ts
+++ src/modules/events/events.gateway.ts
@@ -1,4 +1,5 @@
 import {
+    MessageBody,
     OnGatewayConnection,
     OnGatewayDisconnect,
     SubscribeMessage,
@@ -24,18 +25,57 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     }
 
     @SubscribeMessage('messageToUser')
-    async handleMessageUser(client: any, payload: any) {
+    async handleMessageUser(@MessageBody() payload) {
         this.server.socketsJoin(payload.id);
-        this.server.in(payload.id).emit(`${payload.id}`, {
+        this.server.in(`${payload.id}`).emit(`${payload.id}`, {
+            id: payload.id,
             name: payload.name,
+            role: payload.role,
+            message: payload.message,
         });
     }
 
     @SubscribeMessage('messageToAdmin')
-    async handleMessageAdmin(client: any, payload: any) {
-        this.server.socketsJoin(payload.id);
-        this.server.in(payload.id).emit(`${payload.id}`, {
+    async handleMessageAdmin(@MessageBody() payload) {
+        this.server.emit(`user_sent`, {
+            id: payload.id,
             name: payload.name,
+            role: payload.role,
+            message: payload.message,
+        });
+
+        this.server.emit(`init_user_${payload.id}`, {
+            id: payload.id,
+        });
+    }
+
+    @SubscribeMessage('typing_user')
+    async handleTypingUser(@MessageBody() payload) {
+        this.server.emit(`typing_user_${payload}`, {
+            isType: payload,
+        });
+    }
+
+    @SubscribeMessage('clear_typing_user')
+    async handleClearTypingUser(@MessageBody() payload) {
+        this.server.emit(`clear_typing_user_${payload}`, {
+            isType: payload,
+        });
+    }
+
+    @SubscribeMessage('typing_admin')
+    async handleTypingAdmin(@MessageBody() payload) {
+        this.server.socketsJoin(`typing_admin_${payload}`);
+        this.server.in(`typing_admin_${payload}`).emit(`typing_admin_${payload}`, {
+            isType: payload,
+        });
+    }
+
+    @SubscribeMessage('clear_typing_admin')
+    async handleClearTypingAdmin(@MessageBody() payload) {
+        this.server.socketsJoin(`clear_typing_admin_${payload}`);
+        this.server.in(`clear_typing_admin_${payload}`).emit(`clear_typing_admin_${payload}`, {
+            isType: payload,
         });
     }
 }
