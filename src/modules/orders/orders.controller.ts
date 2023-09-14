793dd82e69caf53366b92557711d413bdea78e17
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2023-09-14 23:57:35 +0700

    add module Orders

--- src/modules/orders/orders.controller.ts
+++ src/modules/orders/orders.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, UsePipes, ValidationPipe, Post, HttpStatus } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { AuthGuard } from '@nestjs/passport';
+import { OrdersAddReqDto } from './dto/orders-add.req.dto';
 
 @Controller('orders')
 export class OrdersController {
     constructor(private ordersService: OrdersService) {}
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('/create')
+    @UsePipes(new ValidationPipe())
+    async createOrder(data: OrdersAddReqDto): Promise<Object> {
+        const result = await this.ordersService.addOrder(data);
+
+        if (result) {
+            return {
+                message: 'success',
+                statusCode: HttpStatus.CREATED,
+                data: result,
+            };
+        } else {
+            return {
+                message: 'error',
+                statusCode: HttpStatus.BAD_REQUEST,
+            };
+        }
+    }
 }
--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { Orders } from './orders.entity';
+import { OrdersUpdateStatusReqDto } from './dto/orders-update-status.req.dto';
+import { OrdersAddReqDto } from './dto/orders-add.req.dto';
 
 @Injectable()
 export class OrdersService {
     constructor(@InjectRepository(Orders) private ordersService: Repository<Orders>) {}
+
+    // get all orders
+    // async getAllOrders(): Promise<Orders[]> {
+    //     return await this.ordersService.createQueryBuilder('orders')
+    //         .addSelect('product.name', 'product_name')
+    //         .addSelect('product.price', 'product_price')
+    //         .
+    // }
+
+    // add new orders (customers)
+    async addOrder(order: OrdersAddReqDto): Promise<Orders> {
+        const newOrder = new Orders();
+        newOrder.product_ = order.product_id;
+        newOrder.customer_ = order.customer_id;
+        newOrder.price = order.price;
+        newOrder.quantity = order.quantity;
+        newOrder.status = order.status;
+
+        return await this.ordersService.save(newOrder);
+    }
+
+    // update status (admin, shiper)
+    async updateStatus(id: number, data: OrdersUpdateStatusReqDto): Promise<UpdateResult> {
+        return await this.ordersService.update(id, data);
+    }
 }
