aa92792aaf0c4401097cc1acac3a7ff4f1fbc8b3
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2024-01-03 17:11:11 +0700

    handle add conversation, add userConversation & check conversation created

--- src/modules/chats/chats.controller.ts
+++ src/modules/chats/chats.controller.ts
@@ -1,9 +1,56 @@
-import { Controller, Post, Get, Param, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Get, Param, Res, HttpStatus, UseGuards, Body, ValidationPipe } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsAddDto } from './dto/chats-add.req.dto';
 import { Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
+import { AddConversation } from './dto/add.conversation.req.dto';
+import { AddUserConversation } from './dto/add.user-conversation.req.dto';
 
 @Controller('chats')
 export class ChatsController {
     constructor(private chatsService: ChatsService) {}
+
+    // conversations
+    @Post('/conversations/create')
+    @UseGuards(AuthGuard('jwt'))
+    async addNewConversation(@Body(new ValidationPipe()) data: AddConversation): Promise<Object> {
+        const conver = await this.chatsService.addConversation(data);
+
+        if (conver) {
+            // add user to chat conversation
+            const userConver = await this.chatsService.addUserConversation({
+                conversation_id: conver.id,
+                customer_id: data.customer_id,
+            });
+
+            if (userConver) {
+                return {
+                    message: 'success',
+                    statusCode: HttpStatus.OK,
+                };
+            }
+        }
+
+        return {
+            message: 'error',
+            statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        };
+    }
+
+    // test
+    @Get('/conversations/test/:id')
+    async checkConversation(@Param('id') id: string) {
+        if (id) {
+            const data = await this.chatsService.checkConversation(+id);
+
+            return {
+                message: 'success',
+                data,
+            };
+        }
+
+        return {
+            message: 'error',
+        };
+    }
 }
--- src/modules/chats/chats.service.ts
+++ src/modules/chats/chats.service.ts
@@ -4,6 +4,8 @@ import { Repository } from 'typeorm';
 import { Conversations } from './conversation.entity';
 import { Messages } from './message.entity';
 import { UserConversations } from './user-conversation.entity';
+import { AddConversation } from './dto/add.conversation.req.dto';
+import { AddUserConversation } from './dto/add.user-conversation.req.dto';
 
 @Injectable()
 export class ChatsService {
@@ -12,4 +14,34 @@ export class ChatsService {
         @InjectRepository(Messages) private message: Repository<Messages>,
         @InjectRepository(UserConversations) private userConver: Repository<UserConversations>,
     ) {}
+
+    // add new conversation
+    async addConversation(data: AddConversation): Promise<Conversations> {
+        const conversation = new Conversations();
+        conversation.conversation_name = data.conversation_name;
+        conversation.conversation_type = data.conversation_type;
+        conversation.created_by_customer_ = data.created_by_customer;
+
+        return await this.conver.save(conversation);
+    }
+
+    // add new user-conversation
+    async addUserConversation(data: AddUserConversation): Promise<UserConversations> {
+        const userConversation = new UserConversations();
+        userConversation.conversation_ = data.conversation_id;
+        userConversation.customer_ = data.customer_id;
+
+        return await this.userConver.save(userConversation);
+    }
+
+    // get id conversation by created user id
+    async checkConversation(data: number) {
+        return await this.conver
+            .createQueryBuilder('conver')
+            .addSelect('userConver.customer_id')
+            .addSelect('conver.created_by_customer_')
+            .innerJoin('userConversations', 'userConver', 'userConver.conversation_id=conver.id')
+            .where('conver.created_by_customer_=:id', { id: data })
+            .getRawOne();
+    }
 }
