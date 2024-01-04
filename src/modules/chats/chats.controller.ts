02c320b876bfa29ed31e853efc8158957bc4ffc4
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2024-01-04 17:11:29 +0700

    handle api add new message, get conversation, get info customer, messages, get customer conversation joined

--- src/modules/chats/chats.controller.ts
+++ src/modules/chats/chats.controller.ts
@@ -5,6 +5,7 @@ import { Response } from 'express';
 import { AuthGuard } from '@nestjs/passport';
 import { AddConversation } from './dto/add.conversation.req.dto';
 import { AddUserConversation } from './dto/add.user-conversation.req.dto';
+import { CheckConversation } from './dto/check.conversation.dto';
 
 @Controller('chats')
 export class ChatsController {
@@ -37,20 +38,93 @@ export class ChatsController {
         };
     }
 
-    // test
-    @Get('/conversations/test/:id')
-    async checkConversation(@Param('id') id: string) {
-        if (id) {
-            const data = await this.chatsService.checkConversation(+id);
+    @Post('/conversations/check')
+    async checkConversation(@Body() obj: CheckConversation) {
+        const data = await this.chatsService.checkConversation(obj);
 
+        if (data) {
             return {
                 message: 'success',
+                statusCode: HttpStatus.OK,
                 data,
             };
         }
 
         return {
             message: 'error',
+            statusCode: HttpStatus.BAD_REQUEST,
+        };
+    }
+
+    @Get('/conversations/info/:id')
+    async getCustomerConversationByCreatedId(@Param('id') id: string): Promise<Object> {
+        const data = await this.chatsService.getCustomerConversationByCreatedId(+id);
+
+        if (data) {
+            return {
+                message: 'success',
+                statusCode: HttpStatus.OK,
+                data,
+            };
+        }
+
+        return {
+            message: 'error',
+            statusCode: HttpStatus.BAD_REQUEST,
+        };
+    }
+
+    @Post('/messages/create')
+    async addNewMessage(@Body() data: any): Promise<Object> {
+        const mes = await this.chatsService.addNewMessageByConversationId(data);
+
+        if (mes) {
+            return {
+                message: 'success',
+                statusCode: HttpStatus.OK,
+                data: mes,
+            };
+        }
+
+        return {
+            message: 'error',
+            statusCode: HttpStatus.BAD_REQUEST,
+        };
+    }
+
+    @Get('/messages/all/:id')
+    async getMessagesByConversationId(@Param('id') id: string): Promise<Object> {
+        const data = await this.chatsService.getMessagesByConversationId(+id);
+
+        if (data) {
+            return {
+                message: 'success',
+                statusCode: HttpStatus.OK,
+                data,
+            };
+        }
+
+        return {
+            message: 'error',
+            statusCode: HttpStatus.BAD_REQUEST,
+        };
+    }
+
+    @Get('/user-conversations/joined/:id')
+    async getJoinedConversationsById(@Param('id') id: string) {
+        const data = await this.chatsService.getJoinedConversationsById(+id);
+
+        if (data) {
+            return {
+                message: 'success',
+                statusCode: HttpStatus.OK,
+                data,
+            };
+        }
+
+        return {
+            message: 'error',
+            statusCode: HttpStatus.BAD_REQUEST,
         };
     }
 }
--- src/modules/chats/chats.service.ts
+++ src/modules/chats/chats.service.ts
@@ -6,6 +6,7 @@ import { Messages } from './message.entity';
 import { UserConversations } from './user-conversation.entity';
 import { AddConversation } from './dto/add.conversation.req.dto';
 import { AddUserConversation } from './dto/add.user-conversation.req.dto';
+import { CheckConversation } from './dto/check.conversation.dto';
 
 @Injectable()
 export class ChatsService {
@@ -34,14 +35,70 @@ export class ChatsService {
         return await this.userConver.save(userConversation);
     }
 
+    // get customer conversation by created id
+    async getCustomerConversationByCreatedId(id: number) {
+        return await this.conver
+            .createQueryBuilder('conver')
+            .addSelect('cus.id')
+            .addSelect('cus.name')
+            .addSelect('cus.avatar_path')
+            .innerJoin('userConversations', 'userConver', 'userConver.conversation_id=conver.id')
+            .innerJoin('customers', 'cus', 'cus.id=userConver.customer_id')
+            .where('conver.created_by_customer_=:id', { id: id })
+            .getRawMany();
+    }
+
     // get id conversation by created user id
-    async checkConversation(data: number) {
+    async checkConversation(data: CheckConversation) {
+        if (data.type) {
+            return await this.conver
+                .createQueryBuilder('conver')
+                .addSelect('userConver.customer_id')
+                .addSelect('conver.created_by_customer_')
+                .innerJoin('userConversations', 'userConver', 'userConver.conversation_id=conver.id')
+                .where('conver.created_by_customer_=:id', { id: data.created_id })
+                .andWhere('userConver.customer_id=:customer_id', { customer_id: data.customer_id })
+                .getRawMany();
+        }
+
         return await this.conver
             .createQueryBuilder('conver')
             .addSelect('userConver.customer_id')
             .addSelect('conver.created_by_customer_')
             .innerJoin('userConversations', 'userConver', 'userConver.conversation_id=conver.id')
-            .where('conver.created_by_customer_=:id', { id: data })
+            .where('conver.created_by_customer_=:id', { id: data.created_id })
+            .andWhere('userConver.customer_id=:customer_id', { customer_id: data.customer_id })
             .getRawOne();
+    }
+
+    // get joined conversations by id customer
+    async getJoinedConversationsById(id: number) {
+        return await this.userConver
+            .createQueryBuilder('userConver')
+            .addSelect('cus.id')
+            .addSelect('cus.avatar_path')
+            .addSelect('cus.name')
+            .innerJoin('conversations', 'conver', 'conver.id=userConver.conversation_id')
+            .innerJoin('customers', 'cus', 'cus.id=conver.created_by_customer_')
+            .where(`userConver.customer_id=${id}`)
+            .getRawMany();
+    }
+
+    // add new message by conversation id
+    async addNewMessageByConversationId(data: any): Promise<Messages> {
+        const message = new Messages();
+        message.conversation_ = data.conversation_id;
+        message.content = data.content;
+        message.sender_ = data.sender_id;
+        message.receiver_ = data.receiver_id;
+
+        return await this.message.save(message);
+    }
+
+    // get messages by conversation id
+    async getMessagesByConversationId(conversationId: number): Promise<Messages[]> {
+        return await this.message.findBy({
+            conversation_: conversationId,
+        });
     }
 }
